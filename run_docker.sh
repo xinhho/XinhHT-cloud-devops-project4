5ab41812ba3d7af1bb076a12a5ed71e6d8f1a5c3
XinhHo <xinhho1995@gmail.com>
2023-06-03 17:48:52 +0700

    update code

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
 ## Complete the following steps to get Docker running locally
+docker login
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build  -t xinhho/cloud-devops-project4 .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -p 8000:80  xinhho/cloud-devops-project4:latest
